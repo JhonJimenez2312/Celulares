1986da009567516b6c6dc67c8549a5a025fa865e
= <=>
2017-09-26 21:25:59 -0500

    spinner funcionando

--- app/src/main/java/com/example/android/celulares/Reportes.java
+++ app/src/main/java/com/example/android/celulares/Reportes.java
@@ -20,19 +20,53 @@ public class Reportes extends AppCompatActivity {
     private Spinner reportes;
     private String op[];
     private Intent in;
+ /*  super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
 
+        res = (TextView)findViewById(R.id.lblResultado);
+        n1 = (EditText)findViewById(R.id.txtNumeroUno);
+        n2 = (EditText)findViewById(R.id.txtNumeroDos);
+        operaciones =(Spinner)findViewById(R.id.cmbOperacionjes);
+        resources = this.getResources();
+        op = resources.getStringArray(R.array.operaciones);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,op);
+        operaciones.setAdapter(adapter);*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_principal);
-
-
-        reportes =(Spinner)findViewById(R.id.cmbReportes);
+        setContentView(R.layout.activity_reportes);
+         reportes =(Spinner)findViewById(R.id.cmbReportes);
         resources = this.getResources();
         op = resources.getStringArray(R.array.reportes);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,op);
         reportes.setAdapter(adapter);
+
+        final int reporteSeleccionado;
+        reporteSeleccionado = reportes.getSelectedItemPosition();
+
+        reportes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                switch (reporteSeleccionado){
+                    case 0:
+                        in= new Intent(Reportes.this,ReporteUno.class);
+                        startActivity(in);
+                        break;
+                    case 1:
+                        in= new Intent(Reportes.this,ReporteDos.class);
+                        startActivity(in);
+                        break;
+                    case 2:
+                        in= new Intent(Reportes.this,ReporteTres.class);
+                        startActivity(in);
+                        break;
+                }
+            }
+        });
+
+
     }
 
     public void mostrarReportes(View v){
